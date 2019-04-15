99abfc92c3da36b75603f029651acef5f0adaf89
Nicolás Miari <nicolasmiari@gmail.com>
2019-04-15 17:16:37 +0900

    Refactor to embedded framework

---
+++ Code/CustomViewController/CustomViewController.h
@@ -0,0 +1,19 @@
+//
+//  CustomViewController.h
+//  CustomViewController
+//
+//  Created by Nicolás Miari on 2019/04/15.
+//  Copyright © 2019 Nicolás Miari. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CustomViewController.
+FOUNDATION_EXPORT double CustomViewControllerVersionNumber;
+
+//! Project version string for CustomViewController.
+FOUNDATION_EXPORT const unsigned char CustomViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomViewController/PublicHeader.h>
+
+
